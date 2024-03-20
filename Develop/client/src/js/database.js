39c27e843e7093da07a71030126f65d3888979c0
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-03-20 18:21:12 -0500

    Called my system and db MTE for my text editor.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('mte', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('mte')) {
+        console.log('mte database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('mte', { keyPath: 'id', autoIncrement: true });
+      console.log('mte database created');
     },
   });
 
